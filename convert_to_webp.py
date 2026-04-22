d2f49ab3cdc6899aa9ff449e330ec37a2ec3359e
Abhinav <abhinavsingh9990604@gmail.com>
2026-04-22 11:37:05 +0530

    Terms and Condition Applied

--- convert_to_webp.py
+++ convert_to_webp.py
@@ -1,4 +1,8 @@
 from __future__ import annotations
+# Copyright (c) 2024, 2025 Oracle and/or its affiliates. All rights reserved.
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
 
 import argparse
 import sys
